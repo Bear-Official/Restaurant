e721f54cf0d8337a118827f703a6c2c2ee5cd56c
Bear <danekgdgdg@gmail.com>
2022-06-08 23:21:37 +0300

    Added slick slider

--- javascript/main.js
+++ javascript/main.js
@@ -1,49 +1,17 @@
-const firstSlider = new Swiper(".first-slider", {
-    // Optional parameters
-    direction: "horizontal",
-    loop: true,
-    slidesPerView: 1,
-});
+$(document).ready(function () {
+    $(".slider-wrapper").slick({
+        arrows: false,
+        infinite: true,
+    });
 
-// Second slider
-const coldSnacks = new Swiper(".cold-snacks", {
-    // Optional parameters
-    mousewheel: {
-        sensitivity: 1,
-    },
-    // Auto play slider
-    // autoplay: {
-    //     delay: 3000,
-    //     stopOnLastSlide: false,
-    //     disableOnInteraction: false,
-    // },
-
-    direction: "horizontal",
-    loop: false,
-    spaceBetween: 16,
-    slidesPerView: 4.2,
-    slidesPerGroup: 4.2,
-
-    breakpoints: {
-        // when window width is >= 320px
-        320: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        // when window width is >= 480px
-        480: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-        },
-        // when window width is >= 640px
-        640: {
-            slidesPerView: 4,
-            spaceBetween: 40,
-        },
-
-        1200: {
-            slidesPerView: 4.2,
-            spaceBetween: 40,
-        },
-    },
+    $(".swiper-wrapper").slick({
+        centerMode: true,
+        centerPadding: "0px",
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        swipeToSlide: true,
+        arrows: false,
+        variableWidth: true,
+        adaptiveHeight: true,
+    });
 });
