15cc779671ca0a3a597c85f670d8fe542cc368b1
Bear <danekgdgdg@gmail.com>
2022-06-08 15:54:08 +0300

    Some bugs has been fixed on site

--- javascript/main.js
+++ javascript/main.js
@@ -12,17 +12,38 @@ const coldSnacks = new Swiper(".cold-snacks", {
         sensitivity: 1,
     },
     // Auto play slider
-    autoplay: {
-        delay: 3000,
-        stopOnLastSlide: false,
-        disableOnInteraction: false,
-    },
+    // autoplay: {
+    //     delay: 3000,
+    //     stopOnLastSlide: false,
+    //     disableOnInteraction: false,
+    // },
 
     direction: "horizontal",
     loop: false,
     spaceBetween: 16,
     slidesPerView: 4.2,
     slidesPerGroup: 4.2,
-});   
 
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
 
+        1200: {
+            slidesPerView: 4.2,
+            spaceBetween: 40,
+        },
+    },
+});
