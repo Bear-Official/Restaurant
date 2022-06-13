9b79c4090dd6f0baef048dbbcda0ffae14db9843
Bear <danekgdgdg@gmail.com>
2022-06-13 19:42:19 +0300

    Added modal window

--- javascript/main.js
+++ javascript/main.js
@@ -15,3 +15,25 @@ $(document).ready(function () {
         adaptiveHeight: true,
     });
 });
+
+$(document).ready(function () {
+    var modalButton = $("[data-toggle=modal]");
+    var closeModalButton = $(".modal__close");
+    modalButton.on("click", openModal);
+    closeModalButton.on("click", closeModal);
+
+    function openModal() {
+        var modalOverlay = $(".modal__overlay");
+        var modalContent = $(".modal__content");
+        modalOverlay.addClass("modal__overlay--visible");
+        modalContent.addClass("modal__content--visible");
+    }
+
+    function closeModal(event) { 
+        event.preventDefault(); 
+        var modalOverlay = $(".modal__overlay");
+        var modalContent = $(".modal__content");
+        modalOverlay.removeClass("modal__overlay--visible");
+        modalContent.removeClass("modal__content--visible");
+    }
+});
