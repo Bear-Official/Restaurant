ec7538fe2012b6f57a416b613db3b38f61b9d177
Bear <danekgdgdg@gmail.com>
2022-06-15 11:37:00 +0300

    Added order section without adaptive

--- javascript/main.js
+++ javascript/main.js
@@ -29,11 +29,70 @@ $(document).ready(function () {
         modalContent.addClass("modal__content--visible");
     }
 
-    function closeModal(event) { 
-        event.preventDefault(); 
+    function closeModal(event) {
+        event.preventDefault();
         var modalOverlay = $(".modal__overlay");
         var modalContent = $(".modal__content");
         modalOverlay.removeClass("modal__overlay--visible");
         modalContent.removeClass("modal__content--visible");
     }
+});
+
+// First livrary
+var btnContainer = document.getElementById("livrary");
+var btns = btnContainer.getElementsByClassName("button");
+var btns = btnContainer.getElementsByClassName("button");
+
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
+// Second livrary
+var btnContainer = document.getElementById("livrary-two");
+
+var btns = btnContainer.getElementsByClassName("btn");
+var btns = btnContainer.getElementsByClassName("btn");
+
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("activ");
+        current[0].className = current[0].className.replace(" activ", "");
+        this.className += " activ";
+    });
+}
+
+// Three livrary
+
+var btnContainer = document.getElementById("when");
+
+var btns = btnContainer.getElementsByClassName("buttonn");
+var btns = btnContainer.getElementsByClassName("buttonn");
+
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("activee");
+        current[0].className = current[0].className.replace(" activee", "");
+        this.className += " activee";
+    });
+}
+
+$(document).ready(function () {
+    $(".minus").click(function () {
+        var $input = $(this).parent().find("input");
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $(".plus").click(function () {
+        var $input = $(this).parent().find("input");
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
 });
