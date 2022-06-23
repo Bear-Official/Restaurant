46c1e3ea43dc883b4737383b7d71a9c739955b0d
Owl <owl.official.mailbox@protonmail.com>
2022-06-23 20:32:56 +0300

    Return

--- javascript/main.js
+++ javascript/main.js
@@ -39,22 +39,22 @@ $(document).ready(function () {
 //     }
 // });
 
-// $(document).ready(function () {
-//     $(".minus").click(function () {
-//         var $input = $(this).parent().find("input");
-//         var count = parseInt($input.val()) - 1;
-//         count = count < 1 ? 1 : count;
-//         $input.val(count);
-//         $input.change();
-//         return false;
-//     });
-//     $(".plus").click(function () {
-//         var $input = $(this).parent().find("input");
-//         $input.val(parseInt($input.val()) + 1);
-//         $input.change();
-//         return false;
-//     });
-// });
+$(document).ready(function () {
+    $(".minus").click(function () {
+        var $input = $(this).parent().find("input");
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $(".plus").click(function () {
+        var $input = $(this).parent().find("input");
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+});
 
 var btnContainer = document.getElementsByClassName("livrary");
 
