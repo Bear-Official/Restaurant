b747b964c76e9b4d45835497fcac71469676797e
Bear <danekgdgdg@gmail.com>
2022-06-28 13:23:03 +0300

    [CSS] stylized

--- javascript/cart.js
+++ javascript/cart.js
@@ -1,50 +1,58 @@
 let d = document,
-    container = document.querySelector('.cart-wrapper'),
-    itemBox = d.querySelectorAll('.item__buy'),
-    cart = d.querySelectorAll('.header__cart')
-
+    container = document.querySelector(".cart-wrapper"),
+    itemBox = d.querySelectorAll(".item__buy"),
+    cart = d.querySelectorAll(".header__cart");
+console.log(itemBox);
 
 function getCartData() {
-    return JSON.parse(localStorage.getItem('cart'));
+    return JSON.parse(localStorage.getItem("cart"));
 }
 
 function setCartData(o) {
-    localStorage.setItem('cart', JSON.stringify(o));
+    localStorage.setItem("cart", JSON.stringify(o));
     return false;
 }
 
 function productsQuantity() {
-    cartData = getCartData()
-    let basketCount = d.querySelectorAll('.cart__count')
+    cartData = getCartData();
+    let basketCount = d.querySelectorAll(".cart__count");
     try {
         for (let i = 0; i < basketCount.length; i++) {
-            basketCount[i].innerHTML = cartData.length
+            basketCount[i].innerHTML = cartData.length;
         }
-
-    } catch (error) {
-    }
+    } catch (error) {}
 }
 
-
 function addToCart() {
-    cartData = getCartData() || [],
-        parentBox = this.parentNode.parentNode.parentNode.parentNode,
-        itemId = parentBox.getAttribute('data-id'),
-        itemTitle = parentBox.querySelector('.item__title').innerHTML,
-        itemPrice = parentBox.querySelector('.item__price').innerHTML,
-        itemDescription = parentBox.querySelector('.item__description').innerHTML,
-        itemImageUrl = parentBox.querySelector('.kitchen').src;
+    console.log("1");
+    (cartData = getCartData() || []),
+        (parentBox = this.parentNode.parentNode.parentNode.parentNode),
+        (itemId = parentBox.getAttribute("data-id")),
+        (itemTitle = parentBox.querySelector(".item__title").innerHTML),
+        (itemPrice = parentBox.querySelector(".item__price").innerHTML),
+        (itemDescription =
+            parentBox.querySelector(".item__description").innerHTML),
+        (itemImageUrl = parentBox.querySelector(".kitchen").src);
 
-    cartData.push([itemId, itemTitle, itemPrice, itemDescription, itemImageUrl, 1]);
-    setCartData(cartData)
-    productsQuantity()
+    cartData.push([
+        itemId,
+        itemTitle,
+        itemPrice,
+        itemDescription,
+        itemImageUrl,
+        1,
+    ]);
+    setCartData(cartData);
+    productsQuantity();
 }
 
 function generateCartProduct() {
-    cartData = getCartData()
+    cartData = getCartData();
     for (let i = 0; i < cartData.length; i++) {
         try {
-            container.insertAdjacentHTML("afterbegin", `<div class="checkout__product" data-id="${cartData[i][0]}">
+            container.insertAdjacentHTML(
+                "afterbegin",
+                `<div class="checkout__product" data-id="${cartData[i][0]}">
                 <div class="product__inner">
                     <div class="product__info">
                         <img class="product__img" src="${cartData[i][4]}" alt="chechout-meat">
@@ -65,41 +73,39 @@ function generateCartProduct() {
                     </div>
                 </div>
             </div>
-                `)
-        } catch (error) {
-
-        }
+                `
+            );
+        } catch (error) {}
     }
 }
 
 function removeProducts(productParent) {
-    cartData = getCartData()
+    cartData = getCartData();
     for (let i = 0; i < cartData.length; i++) {
         if (productParent.dataset.id == cartData[i][0]) {
-            cartData.pop()
+            cartData.pop();
         }
     }
-    productParent.remove()
-    setCartData(cartData)
-    productsQuantity(localStorage.getItem("cart").length)
+    productParent.remove();
+    setCartData(cartData);
+    productsQuantity(localStorage.getItem("cart").length);
 }
 
 function cartModal() {
-    cartData = getCartData(),
-        modalButton = $("[data-toggle=modal]"),
-        closeModalButton = $(".modal__close"),
-        modalOverlay = $(".modal__overlay"),
-        modalContent = $(".modal__content");
+    (cartData = getCartData()),
+        (modalButton = $("[data-toggle=modal]")),
+        (closeModalButton = $(".modal__close")),
+        (modalOverlay = $(".modal__overlay")),
+        (modalContent = $(".modal__content"));
 
     if (cartData.length !== 0) {
-
         for (let i = 0; i < modalButton.length; i++) {
-            modalButton.off('click', openModal);
-            modalButton[i].href = "checkout.html"
+            modalButton.off("click", openModal);
+            modalButton[i].href = "checkout.html";
         }
     } else {
         for (let i = 0; i < modalButton.length; i++) {
-            modalButton[i].href = "#"
+            modalButton[i].href = "#";
         }
         modalButton.on("click", openModal);
         closeModalButton.on("click", closeModal);
@@ -117,22 +123,22 @@ function cartModal() {
 }
 
 try {
-    container.addEventListener('click', (e) => {
-        if (e.target.classList.contains('product__button--remove')) {
-            removeProducts(e.target.closest('.checkout__product'))
+    container.addEventListener("click", (e) => {
+        if (e.target.classList.contains("product__button--remove")) {
+            removeProducts(e.target.closest(".checkout__product"));
         }
-    })
-    productsQuantity(localStorage.getItem("cart").length)
-} catch (error) {
-
-}
+    });
+    productsQuantity(localStorage.getItem("cart").length);
+} catch (error) {}
 
 for (var i = 0; i < itemBox.length; i++) {
-    itemBox[i].addEventListener("click", addToCart)
+    itemBox[i].addEventListener("click", addToCart);
 }
 
 for (var i = 0; i < cart.length; i++) {
-    cart[i].addEventListener("click", cartModal)
+    cart[i].addEventListener("click", cartModal);
 }
 
-window.onload = () => { generateCartProduct(), productsQuantity(), cartModal() }
+window.onload = () => {
+    generateCartProduct(), productsQuantity(), cartModal();
+};
